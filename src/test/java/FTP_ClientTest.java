a40e70d2be03ae7aa3a7d6b6d2e17576d0b1db59
Chinmay Tawde <chinmaytawde15@gmail.com>
2021-07-08 18:28:34 -0700

    Added Dummy Junit Test case

---
+++ src/test/java/FTP_ClientTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FTP_ClientTest {
+
+    @Test
+    public void test_JUnit() {
+        String str1="This is the testcase in this class";
+        assertEquals("This is the testcase in this class", str1);
+    }
+
+}
