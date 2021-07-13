b324e0370a4747a1f6fc768202c94f6b4f52e5d3
Chinmay Tawde <chinmaytawde15@gmail.com>
2021-07-12 22:09:56 -0700

    Removed FTPSConnection class

--- src/main/java/ftp/core/RemoteConnectionFactory.java
+++ src/main/java/ftp/core/RemoteConnectionFactory.java
@@ -3,12 +3,10 @@ package ftp.core;
 public class RemoteConnectionFactory {
 
     public RemoteConnection getInstance(String protocol) {
-        if (protocol.equalsIgnoreCase("FTP")) {
-            return new FTPConnection();
-        } else if (protocol.equalsIgnoreCase("FTPS")) {
-            return new FTPSConnection();
-        } else {
+        if (protocol.equalsIgnoreCase("SFTP")) {
             return new SFTPConnection();
+        }  else {
+            return new FTPConnection();
         }
     }
 }
