4cf62c486d30f9d7f48eda13cadbb159b5db1fbf
Sree-Vandana <sreevandana.sv@gmail.com>
2021-07-18 23:38:39 -0700

    comments in unit test file

--- src/test/java/FTP_ClientTest.java
+++ src/test/java/FTP_ClientTest.java
@@ -1,7 +1,15 @@
+import ftp.core.FTPConnection;
+import org.apache.commons.net.ftp.FTPClient;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Unit tests for {@link FTPClient} class.
+ */
 public class FTP_ClientTest {
 
     @Test
@@ -9,5 +17,4 @@ public class FTP_ClientTest {
         String str1="This is the testcase in this class";
         assertEquals("This is the testcase in this class", str1);
     }
-
 }
